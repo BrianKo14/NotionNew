29fe26330baf7383c7cd469b4739580366de5544
BrianKo14 <23661488+BrianKo14@users.noreply.github.com>
2022-12-27 20:34:56 -0300

    Block handles

--- react/src/App.js
+++ react/src/App.js
@@ -1,13 +1,23 @@
+import { useState, useEffect } from 'react';
+
 import static_navigationbar_right from './media/static-navigationbar-right.png';
 import static_navigationbar_left from './media/static-navigationbar-left.png';
-import page_icon from './media/office-building_1f3e2.png';
+import page_icon from './media/page-icon.png';
+
+import add_button from './media/add-button.png';
+import drag_button from './media/drag-button.png';
 
 import './App.css';
 
-// Fonts: paragraph (0), headings (1, 2, 3), title (4)
-const fontSizes = ['16px', '1.875em', '1.5em', '1.25em', '40px'];
-const fontWeights = ['normal', '600', '600', '600', '700'];
-const fontMargins = ['0px', '2em', '1.4em', '1em', '0px']
+const FONTS = {
+  'paragraph': {'size': '16px', 'weight': 'normal', 'margin': '0px'},
+  'heading1': {'size': '1.875em', 'weight': '600', 'margin': '2em'},
+  'heading2': {'size': '1.5em', 'weight': '600', 'margin': '1.8em'},
+  'heading3': {'size': '1.25em', 'weight': '600', 'margin': '1em'},
+  'title': {'size': '40px', 'weight': '700', 'margin': '0'}
+};
+
+const MOUSE_HOVER_OFFSET = 120;
 
 
 /* NAVIGATION BAR */
@@ -24,17 +34,63 @@ function NavigationBar() {
 
 /* DOCUMENT */
 
-function TextBox(props) {
+function useMousePosition() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
+  useEffect(() => {
+    function handleMouseMove(event) {
+      setPosition({ x: event.clientX, y: event.clientY });
+    }
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return position;
+}
+
+function BlockSideHandle() {
   return (
-    <div className="textbox">
-      <input type="text" defaultValue={props.text} style={{ 
-        fontSize: fontSizes[props.size],
-        fontWeight: fontWeights[props.size],
-        marginTop: fontMargins[props.size]
-      }} />
+    <div className="block-side-handle">
+      <img src={add_button}/>
+      <img src={drag_button}/>
     </div>
   );
+}
+
+function TextBox(props) {
+
+  const [isHovering, setIsHovering] = useState(false);
+  const mousePos = useMousePosition();
+
+  return <div className="textbox"
+      
+      style={{
+        height: FONTS[props.size].size,
+        marginTop: FONTS[props.size].margin,
+      }}
+      
+      // Show side handle when hovering over textbox + offset
+      ref={el => {
+        if (el) {
+          const rect = el.getBoundingClientRect();
+          setIsHovering(mousePos.x > rect.left - MOUSE_HOVER_OFFSET && mousePos.x < rect.left + rect.width + MOUSE_HOVER_OFFSET &&
+                        mousePos.y > rect.top && mousePos.y < rect.top + rect.height);
+        }
+      }}>
+
+    { isHovering && props.size != "title" ? <BlockSideHandle /> : null }
+
+    <input type="text" defaultValue={props.text}
+      style={{ 
+        fontSize: FONTS[props.size].size,
+        fontWeight: FONTS[props.size].weight,
+      }} />
+
+  </div>
 }
 
 function Document() {
@@ -42,11 +98,13 @@ function Document() {
     <div id="document">
       {/* Header */}
       <img id="page-icon" src={page_icon} />
-      <TextBox size={4} text="Job Application Projects" />
+      <TextBox size="title" text="Job Application Projects" />
 
       {/* Body */}
-      <TextBox size={2} text="Example" />
-      <TextBox size={0} text="Some paragraph text" />
+      <TextBox size="heading2" text="Example" />
+      <TextBox size="paragraph" text="Some paragraph text" selected={true} />
+      <TextBox size="paragraph" text="More paragraph text" selected={true} />
+      <TextBox size="heading1" text="Heading1" selected={true} />
     </div>
   );
 }
