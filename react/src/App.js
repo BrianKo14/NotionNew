ebb58473152cc68880f6bee87b39603cbe272107
BrianKo14 <23661488+BrianKo14@users.noreply.github.com>
2022-12-29 23:34:32 -0300

    Add new blocks

--- react/src/App.js
+++ react/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import static_navigationbar_right from './media/static-navigationbar-right.png';
 import static_navigationbar_left from './media/static-navigationbar-left.png';
@@ -10,14 +10,26 @@ import drag_button from './media/drag-button.png';
 import './App.css';
 
 const FONTS = {
-  'paragraph': {'size': '16px', 'weight': 'normal', 'margin': '0px'},
-  'heading1': {'size': '1.875em', 'weight': '600', 'margin': '2em'},
-  'heading2': {'size': '1.5em', 'weight': '600', 'margin': '1.8em'},
-  'heading3': {'size': '1.25em', 'weight': '600', 'margin': '1em'},
-  'title': {'size': '40px', 'weight': '700', 'margin': '0'}
+  'paragraph': {'size': '16px', 'weight': 'normal', 'margin': '0px', 'placeholder': 'Type "/" for commands'},
+  'heading1': {'size': '1.875em', 'weight': '600', 'margin': '2em', 'placeholder': "Heading 1"},
+  'heading2': {'size': '1.5em', 'weight': '600', 'margin': '1.8em', 'placeholder': "Heading 2"},
+  'heading3': {'size': '1.25em', 'weight': '600', 'margin': '1em', 'placeholder': "Heading 3"},
+  'title': {'size': '40px', 'weight': '700', 'margin': '0', 'placeholder': ""}
 };
 
 const MOUSE_HOVER_OFFSET = 120;
+
+
+/* APP */
+
+function App() {
+  return (
+    <div className="App">
+      <Document />
+      <NavigationBar />
+    </div>
+  );
+}
 
 
 /* NAVIGATION BAR */
@@ -34,6 +46,93 @@ function NavigationBar() {
 
 /* DOCUMENT */
 
+function Document() {
+  
+  const [textBoxes, setTextBoxes] = useState([
+    {"size": "heading2", "text": "Example"},
+    {"size": "paragraph", "text": "Some paragraph text"},
+    {"size": "paragraph", "text": "More paragraph text"},
+    {"size": "heading1", "text": "Heading1"}
+  ]);
+
+  return (
+    <div id="document">
+      {/* Header */}
+      <img id="page-icon" src={page_icon} />
+      <TextBox size="title" text="Job Application Projects" />
+
+      {/* Body */}
+      {
+        textBoxes.map(box => {
+          return <TextBox size={box.size} text={box.text}
+                  textBoxes={textBoxes} setTextBoxes={setTextBoxes} />;
+        })
+      }
+    </div>
+  );
+}
+
+
+/* TEXTBOX */
+
+function TextBox(props) {
+
+  // Control side handle visibility with mouse position
+  const [isHovering, setIsHovering] = useState(false);
+  const mousePos = useMousePosition();
+
+  // Focus on new blocks
+  const focusOnMount = useRef(null);
+  useEffect(() => { focusOnMount.current.focus(); }, []);
+
+
+  return <div className="textbox"
+      
+        style={{
+          height: FONTS[props.size].size,
+          marginTop: FONTS[props.size].margin,
+        }}
+        
+        // Show side handle when hovering over textbox + offset
+        ref={el => { if (el) controlSideHandle(el, mousePos, setIsHovering) }}>
+
+
+    {/* Side handle */}
+    { isHovering && props.size !== "title" ? 
+      <BlockSideHandle textBoxes={props.textBoxes} setTextBoxes={props.setTextBoxes} />
+    : null }
+
+    {/* Input */}
+    <input type="text" defaultValue={props.text}
+      style={{ 
+        fontSize: FONTS[props.size].size,
+        fontWeight: FONTS[props.size].weight,
+      }} 
+
+      // Placeholder
+      onBlur={e => controlPlaceholder(false, e.target, FONTS[props.size].placeholder)}
+      onFocus={e => controlPlaceholder(true, e.target, FONTS[props.size].placeholder)}
+      onChange={e => controlPlaceholder(true, e.target, FONTS[props.size].placeholder)}
+      ref={focusOnMount}
+    />
+
+  </div>
+}
+
+function BlockSideHandle(props) {
+  return (
+    <div className="block-side-handle">
+      <img src={add_button} onClick={() => addTextBox(props.textBoxes, props.setTextBoxes)} />
+      <img src={drag_button} />
+    </div>
+  );
+}
+
+
+/* AUXILIARIES */
+
+/** Gets mouse position to detect hovering for the BlockSideHandle.
+ * By doing this in JS instead of CSS I can easily add an artifical offset to the hover area. */
 function useMousePosition() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
@@ -52,73 +151,35 @@ function useMousePosition() {
   return position;
 }
 
-function BlockSideHandle() {
-  return (
-    <div className="block-side-handle">
-      <img src={add_button}/>
-      <img src={drag_button}/>
-    </div>
-  );
+/** Adds a new block. I will not implement delete. */
+function addTextBox(textBoxes, setTextBoxes) {
+  const tmp = [...textBoxes];
+  tmp.push({"size": "paragraph", "text": ""});
+  console.log(tmp);
+  setTextBoxes(tmp);
 }
 
-function TextBox(props) {
-
-  const [isHovering, setIsHovering] = useState(false);
-  const mousePos = useMousePosition();
-
-  return <div className="textbox"
-      
-      style={{
-        height: FONTS[props.size].size,
-        marginTop: FONTS[props.size].margin,
-      }}
-      
-      // Show side handle when hovering over textbox + offset
-      ref={el => {
-        if (el) {
-          const rect = el.getBoundingClientRect();
-          setIsHovering(mousePos.x > rect.left - MOUSE_HOVER_OFFSET && mousePos.x < rect.left + rect.width + MOUSE_HOVER_OFFSET &&
-                        mousePos.y > rect.top && mousePos.y < rect.top + rect.height);
-        }
-      }}>
-
-    { isHovering && props.size != "title" ? <BlockSideHandle /> : null }
-
-    <input type="text" defaultValue={props.text}
-      style={{ 
-        fontSize: FONTS[props.size].size,
-        fontWeight: FONTS[props.size].weight,
-      }} />
-
-  </div>
+/**
+ * Controls the placeholder of the input textbox.
+ * For paragraphs: it shows only when empty and selected.
+ * For headings: it shows always when empty.
+*/
+function controlPlaceholder(selected, target, placeholder) {
+  if (selected && target.value === "") {
+    target.placeholder = placeholder;
+  } else if (!placeholder.includes("Heading")) {
+    target.placeholder = "";
+  }
 }
 
-function Document() {
-  return (
-    <div id="document">
-      {/* Header */}
-      <img id="page-icon" src={page_icon} />
-      <TextBox size="title" text="Job Application Projects" />
-
-      {/* Body */}
-      <TextBox size="heading2" text="Example" />
-      <TextBox size="paragraph" text="Some paragraph text" selected={true} />
-      <TextBox size="paragraph" text="More paragraph text" selected={true} />
-      <TextBox size="heading1" text="Heading1" selected={true} />
-    </div>
-  );
+function controlSideHandle(el, mousePos, setIsHovering) {
+  const rect = el.getBoundingClientRect();
+  setIsHovering(mousePos.x > rect.left - MOUSE_HOVER_OFFSET &&
+                mousePos.x < rect.left + rect.width + MOUSE_HOVER_OFFSET &&
+                mousePos.y > rect.top &&
+                mousePos.y < rect.top + rect.height);
 }
 
 
-/* APP */
-
-function App() {
-  return (
-    <div className="App">
-      <Document />
-      <NavigationBar />
-    </div>
-  );
-}
 
 export default App;
