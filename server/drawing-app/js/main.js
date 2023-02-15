a6dcac15829e8fe0120aade90bd30131402010d1
BrianKo14 <23661488+BrianKo14@users.noreply.github.com>
2023-02-15 19:15:32 -0300

    Drawing app design

--- server/drawing-app/js/main.js
+++ server/drawing-app/js/main.js
@@ -4,5 +4,5 @@ const fabricCanvas = new fabric.Canvas(canvas);
 fabricCanvas.isDrawingMode = true;
 fabricCanvas.freeDrawingBrush.width = 5;
 
-fabricCanvas.setWidth(window.innerWidth);
-fabricCanvas.setHeight(window.innerHeight);
+fabricCanvas.setWidth(window.innerWidth - 60);
+fabricCanvas.setHeight(window.innerHeight - 150 - 60 - 30);
