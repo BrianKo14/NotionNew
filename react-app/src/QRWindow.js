315dbbc4b8277ac12fa0e5e895c39a9e06646e3c
BrianKo14 <23661488+BrianKo14@users.noreply.github.com>
2023-02-21 21:04:01 -0300

    Design improvements

--- react-app/src/QRWindow.js
+++ react-app/src/QRWindow.js
@@ -18,7 +18,12 @@ function QRWindow(props) {
 			console.log(url); // FIXME: makes two requests to server
 
 			try {
-				const dataUri = await QRCode.toDataURL(url, { width: 256, height: 256 });
+				const dataUri = await QRCode.toDataURL(url, { 
+					width: 256, 
+					height: 256, 
+					margin: 0,
+					color: { dark: '#000000', light: '#F7F7F5'}
+				});
 				setQRCodeDataUri(dataUri);
 
 				startPolling(() => getDrawingAfterPolling(props));
@@ -37,7 +42,7 @@ function QRWindow(props) {
 				transform: props.positionFromTop > 0.5 ? 'translate(2%, -105%)' : 'translate(2%, -90%)'
 			}}
 		>
-			<div className="qrwindow-header">Scan this QR code with your mobile device and make a drawing!</div>
+			<div className="qrwindow-header">Scan this QR code with your <br /><span>mobile device</span> and make a drawing!</div>
 			<img className="qrwindow-img" src={qrCodeDataUri}></img>
 		</div>
 	);
