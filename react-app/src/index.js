8d9ab016eaaeb20ae6f2bd4b6c716e100a7702b6
BrianKo14 <23661488+BrianKo14@users.noreply.github.com>
2024-06-13 23:56:31 -0300

    New resume + non-dekstop warning

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+/* Mobile devices */
+const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+
+const MobileView = () => (
+  <div><h1>For the best experience, please access this site from a desktop computer.</h1></div> );
+
+
+/* RENDER */
 root.render(
   <React.StrictMode>
-    <App />
+    { isMobile ? <MobileView /> : <App /> }
   </React.StrictMode>
 );
